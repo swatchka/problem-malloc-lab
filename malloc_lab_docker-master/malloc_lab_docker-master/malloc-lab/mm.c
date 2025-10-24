717278168d8a8397baa984bd8bab1d6325cceb5a
swatchka <yoonsung2007@gmail.com>
2025-10-24 23:26:23 +0900

    23:26

--- malloc_lab_docker-master/malloc_lab_docker-master/malloc-lab/mm.c
+++ malloc_lab_docker-master/malloc_lab_docker-master/malloc-lab/mm.c
@@ -34,28 +34,78 @@ team_t team = {
     /* Second member's email address (leave blank if none) */
     "qrxoghks1209@gmail.com"
 };
-
+/* | Header | Payload ... | Footer | */
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7) // 정렬용
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // 사용자 요청 크기 정의
-#define WSIZE 4 // 워드사이즈 4바이트로 정의
-#define DSIZE 8 // 더블워드사이즈 8바이트로 정의
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // 요청 크기 계산시 보조용
+#define WSIZE 4 // header/footer 1워드 크기
+#define DSIZE 8 // 최소 블록 크기 (더블워드)
 #define CHUNKSIZE (1<<12) // 힙을 확장하는 기본 단위
 
-#define MAX(x, y) ((x) > (y)? (x) : (y)) // x와 y중 더 큰 값을 반환
+#define MAX(x, y) ((x) > (y)? (x) : (y)) // x와 y중 더 큰 값을 반환(둘중 더 큰값 선택)
+/* extend_heap()에서 heap 확장 크기 결정 */
+
+#define PACK(size, alloc) ((size) | (alloc)) // header/footer 값 구성
+/* 크기 + 할당여부 결합*/
+
+#define GET(p) (*(unsigned int *)(p)) // 메모리 접근 
+#define PUT(p, val) (*(unsigned int *)(p) = (val)) // 메모리 접근
+/* header/footer 값 읽고 쓰기*/
+
+#define GET_SIZE(p) (GET(p) & ~0x7) // header/footer에서 크기 추출
+#define GET_ALLOC(p) (GET(p) & 0x1) // header/footer에서 할당 상태 추출
+
+#define HDRP(bp) ((char *)(bp) - WSIZE) 
+// bp = 0x1008 → HDRP(bp) = 0x1008 - 4 = 0x1004
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+/* payload -> header/footer 주소 계산 */
+
+#define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) // 다음 블록으로 이동
+#define PREV_BLKP(bp) ((char*)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) // 이전 블록으로 이동
 /*
  * mm_init - initialize the malloc package.
  */
+static void *heap_listp;
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1) {
+        return NULL;
+    }
+
+    PUT(HDRP(bp), PACK(size, 0));           
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
+}
+
+
 int mm_init(void)
 {
     // 힙을 넓혀주는 친구
     // 힙의 최소한의 논리적 구조를 만들어 주는 역할
     // 첫번째 free block도 만들어 둘것
-    mem_sbrk(4 * WSIZE);
+    heap_listp = mem_sbrk(4 * WSIZE);
+    if (heap_listp == (void *) -1){
+        return -1;
+    }
+    PUT(heap_listp, 0);                             // 패딩
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));    // 프롤로그 헤더
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));    // 프롤로그 푸터
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));        // 에필로그 헤더
+    heap_listp += (2*WSIZE);                        // 포인터 조정  
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL) {
+        return -1;
+    }
     return 0;
 }
 
